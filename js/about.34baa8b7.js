2465273ea90e9afe82d0f28133ac4c4dcf508722
Kris Tung <ragdoll.washoku89@gmail.com>
2022-11-11 16:01:06 +0800

    deploy

---
+++ js/about.34baa8b7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_cli_demo"]=self["webpackChunkvue_cli_demo"]||[]).push([[443],{381:function(e,n,u){u.r(n),u.d(n,{default:function(){return d}});var t=u(252);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(e,n){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=u(744);const o={},l=(0,i.Z)(o,[["render",s]]);var d=l}}]);
+//# sourceMappingURL=about.34baa8b7.js.map
