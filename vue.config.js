418a670553d821f69e361c46691c43e5bc321e15
Kris Tung <ragdoll.washoku89@gmail.com>
2022-11-11 15:57:01 +0800

    add deploy & vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-cli-demo/' // vue-cli-demo 為 repo 名稱
+    : '/'
+}
